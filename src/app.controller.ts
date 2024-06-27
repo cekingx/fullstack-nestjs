a6a7903977d3df6fa967914146432c141aab2536
Dirga Yasa <cekingx@gmail.com>
2024-06-27 11:35:36 +0800

    feat: initialize

---
+++ src/app.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  @Get()
+  @Render('index')
+  root() {
+    return { message: 'Hello worlddd!' };
+  }
+}
